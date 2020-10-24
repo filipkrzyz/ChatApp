657e486bfb5abb644bf889f486fa0ed96e2cddf6
filipkrzyz <krzyzanowski.f@gmail.com>
2020-10-24 11:36:13 +0200

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Filip Krzyzanowski on 24/10/2020.
+//  Copyright © 2020 Filip Krzyzanowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
